5e6abd0ed0d651f4da3e9cd61b3311f853fbfb96
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2025-01-05 11:54:25 +0600

    tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   // eslint-disable-next-line no-undef
+//   plugins: [require("daisyui")],
+// };
+
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
